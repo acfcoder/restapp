3edc4b2eaa7ecf1996439955be438cbc6eb8e3f0
Lebrel Code <lebrelcode@gmail.com>
2024-06-10 02:41:28 +0200

    Server Product interface created

---
+++ restApp/server/src/products/product.ts
@@ -0,0 +1,18 @@
+import * as mongodb from "mongodb";
+
+export interface Product {
+    name: string;
+    desc: string; // description
+    l_desc: string; // long description
+    price: number;
+    category: number;
+    img: string;
+    pos: number; // position
+    available: boolean
+    time: number; // preparation time
+    excl: number; // percentage of exclusivity
+    allergens: number[];
+    tags: string[];
+    _id?: mongodb.ObjectId;
+
+}
