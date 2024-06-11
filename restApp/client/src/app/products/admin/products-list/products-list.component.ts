5d97ce9e60b901c4dedde3909bef9ee8c1215e0b
Lebrel Code <lebrelcode@gmail.com>
2024-06-11 11:07:42 +0200

    making product.service and admin/products/edit

--- restApp/client/src/app/products/admin/products-list/products-list.component.ts
+++ restApp/client/src/app/products/admin/products-list/products-list.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Product } from '../../product';
+import { ProductService } from '../../product.service';
 
 @Component({
-  selector: 'app-products-list',
+  selector: 'app-admin-products-list',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      products-list works!
-    </p>
-  `,
+  imports: [RouterModule, MatTableModule, MatButtonModule, MatCardModule],
+  templateUrl: 'products-list.component.html',
   styles: ``
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
+  products$ = {} as WritableSignal<Product[]>;
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.fetchProducts()
+  }
+
+  deleteProduct(id: string): void {
+    this.productService.deleteProduct(id).subscribe({
+      next: () => this.fetchProducts()
+    })
+  }
+
+  private fetchProducts(): void {
+    this.products$ = this.productService.products$;
+    this.productService.getProducts();
+  }
 }
--- restApp/client/src/app/products/product.service.ts
+++ restApp/client/src/app/products/product.service.ts
@@ -26,6 +26,24 @@ export class ProductService {
     return this.products$()
   }
 
+  getProduct(id: string) {
+    this.httpClient.get<Product>(`${this.url}/menu/${id}`)
+    .subscribe(product => {
+      this.product$.set(product)
+      return this.product$()
+    })
+  }
 
+  createProduct(product: Product) {
+    return this.httpClient.post(`${this.url}/admin/products`, product, {responseType: 'text'});
+  }
+
+  updateProduct(id: string, product: Product) {
+    return this.httpClient.put(`${this.url}/admin/products/${id}`, product, {responseType: 'text'});
+  }
+
+  deleteProduct(id: string){
+    return this.httpClient.delete(`${this.url}/admin/products/4{id}`, {responseType: 'text'});
+  }
 
 }
